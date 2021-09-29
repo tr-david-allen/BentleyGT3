4f7035abbd9ec5663771551472108568e33d1e88
tr-david-allen <david@turnracing.com>
2021-09-29 11:32:48 -0700

    Add project files.

---
+++ Bentley/Controls/SliderButton.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bentley.Controls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Bentley.Controls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Bentley.Controls;assembly=Bentley.Controls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:SliderButton/>
+    ///
+    /// </summary>
+    public class SliderButton : System.Windows.Controls.Primitives.ToggleButton
+    {
+        public double ButtonWidth
+        {
+            get
+            {
+                return (double)GetValue(ButtonWidthProperty);
+            }
+            set
+            {
+                SetValue(ButtonWidthProperty, value);
+            }
+        }
+
+        public static readonly System.Windows.DependencyProperty ButtonWidthProperty =
+               System.Windows.DependencyProperty.Register("ButtonWidth", typeof(double),
+               typeof(SliderButton), new System.Windows.PropertyMetadata(0.0));
+
+        public string OnLabel
+        {
+            get
+            {
+                return (string)GetValue(OnLabelProperty);
+            }
+            set
+            {
+                SetValue(OnLabelProperty, value);
+            }
+        }
+
+        public static readonly System.Windows.DependencyProperty
+               OnLabelProperty = System.Windows.DependencyProperty.Register
+               ("OnLabel", typeof(string), typeof(SliderButton),
+               new System.Windows.PropertyMetadata(""));
+
+        public string OffLabel
+        {
+            get
+            {
+                return (string)GetValue(OffLabelProperty);
+            }
+            set
+            {
+                SetValue(OffLabelProperty, value);
+            }
+        }
+
+        public static readonly System.Windows.DependencyProperty OffLabelProperty =
+               System.Windows.DependencyProperty.Register("OffLabel", typeof(string),
+               typeof(SliderButton), new System.Windows.PropertyMetadata(""));
+    }
+}
---
+++ Bentley/ViewModels/MainViewModel.cs
@@ -0,0 +1,143 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Bentley.ViewModels
+{
+    class MainViewModel : Screen
+    {
+        private bool functionToggle = true;
+        private string rotarySwitchFunctionLabel;
+        private string toggleLeftLabel;
+        private string toggleRightLabel;
+        private string titleLabel;
+        private string buttonLogTextBox;
+        private string buttonLogLabel;
+        private SerialPort sp;
+        private byte[] byteArray = new byte[1];
+
+
+        public bool FunctionToggle
+        {
+            get => functionToggle;
+            set
+            {
+                functionToggle = value;
+                NotifyOfPropertyChange(nameof(FunctionToggle));
+            }
+        }
+
+        public string RotarySwitchFunctionLabel
+        {
+            get => rotarySwitchFunctionLabel;
+            set
+            {
+                rotarySwitchFunctionLabel = value;
+                NotifyOfPropertyChange(nameof(RotarySwitchFunctionLabel));
+            }
+        }
+
+        public string ToggleLeftLabel
+        {
+            get => toggleLeftLabel;
+            set
+            {
+                toggleLeftLabel = value;
+                NotifyOfPropertyChange(nameof(ToggleLeftLabel));
+            }
+        }
+
+        public string ToggleRightLabel
+        {
+            get => toggleRightLabel;
+            set
+            {
+                toggleRightLabel = value;
+                NotifyOfPropertyChange(nameof(ToggleRightLabel));
+            }
+        }
+
+        public string TitleLabel
+        {
+            get => titleLabel;
+            set
+            {
+                titleLabel = value;
+                NotifyOfPropertyChange(nameof(TitleLabel));
+            }
+        }
+
+        public string ButtonLogTextBox
+        {
+            get => buttonLogTextBox;
+            set
+            {
+                buttonLogTextBox = value;
+                NotifyOfPropertyChange(nameof(ButtonLogTextBox));
+            }
+        }
+
+        public string ButtonLogLabel
+        {
+            get => buttonLogLabel;
+            set
+            {
+                buttonLogLabel = value;
+                NotifyOfPropertyChange(nameof(ButtonLogLabel));
+            }
+        }
+
+        public void Save()
+        {
+            if (FunctionToggle)
+            {
+                sp.Write("0");
+            }
+            else
+            {
+                sp.Write("1");
+            }
+        }
+
+        public MainViewModel()
+        {
+            functionToggle = false;
+            RotarySwitchFunctionLabel = "Rotary Switch Function";
+            ToggleLeftLabel = "12 Positions";
+            ToggleRightLabel = "Up and Down";
+            TitleLabel = "Button Plate Configurator";
+            ButtonLogLabel = "Button Log:";
+            ButtonLogTextBox = "Button 1 Pressed";
+            var portName = "COM4";
+            sp = new SerialPort(portName);
+            sp.DtrEnable = true;
+            sp.BaudRate = 115200;
+            sp.Parity = Parity.None;
+            sp.StopBits = StopBits.One;
+            sp.DataBits = 8;
+            sp.Open();
+            sp.DataReceived += Sp_DataReceived;
+            ButtonLogTextBox = "Connected!";
+
+            sp.Write("C");
+        }
+
+        private void Sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var serialDevice = sender as SerialPort;
+            var bytes = new byte[serialDevice.BytesToRead];
+            var buffer = new byte[serialDevice.BytesToRead];
+            ButtonLogTextBox = serialDevice.ReadLine();
+
+            // process data on the GUI thread
+            Application.Current.Dispatcher.Invoke(new System.Action(() => {
+                //... do something here ...
+            }));
+        }
+    }
+}
